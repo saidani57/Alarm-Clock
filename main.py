b13abfe400ecb3856ea5fd86ae1dc8d109bee6ce
saidani57 <saidanianis763@gmail.com>
2023-02-26 14:29:54 +0100

    basic functionality of the alarm + GUI work

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+from tkinter import *
+import datetime
+import time
+import winsound
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def alarm(set_alarm_timer):
+    while True:
+        time.sleep(1)
+        current_time = datetime.datetime.now()
+        now = current_time.strftime("%H:%M:%S")
+        date = current_time.strftime("%d/%m/%Y")
+        print("The Set Date is:",date)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        print(now)
+        print(set_alarm_timer)
+        if now == set_alarm_timer:
+            print("Time to Wake up")
+            winsound.PlaySound("sound.wav",winsound.SND_ASYNC)
+            time.sleep(0.5)
+            quit()
+
+def get_time():
+    set_alarm_timer = f"{hour.get()}:{min.get()}:{sec.get()}"
+    alarm(set_alarm_timer)
+
+clock = Tk()
+
+clock.title("Alarm Clock")
+clock.geometry("480x260")
+time_format=Label(clock, text= "Enter time in 24 hour format!", fg="red",bg="black",font="Arial").place(x=60,y=120)
+addTime = Label(clock,text = "Hour  Min   Sec",font=60).place(x = 110)
+setYourAlarm = Label(clock,text = "wake up",fg="blue",relief = "solid",font=("Helevetica",7,"bold")).place(x=0, y=29)
+
+# The Variables we require to set the alarm(initialization):
+hour = StringVar()
+min = StringVar()
+sec = StringVar()
+
+#Time required to set the alarm clock:
+hourTime= Entry(clock,textvariable = hour,bg = "pink",width = 15).place(x=110,y=30)
+minTime= Entry(clock,textvariable = min,bg = "pink",width = 15).place(x=150,y=30)
+secTime = Entry(clock,textvariable = sec,bg = "pink",width = 15).place(x=200,y=30)
+
+#To take the time input by user:
+submit = Button(clock,text = "Set Alarm",fg="red",width = 10,command = actual_time).place(x =110,y=70)
+
+clock.mainloop()
+#Execution of the window.
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    actual_time()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
